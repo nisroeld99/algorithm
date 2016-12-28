a980b450fad70d5accd778bdd99b30431023d3c1
nisroeld99 <hellowworld1990@gmail.com>
2016-12-28 15:53:47 +0900

    picnic

---
+++ src/JMBook/picinc.cpp
@@ -0,0 +1,76 @@
+/*
+ * picinc.cpp
+ *
+ *
+ */
+
+
+/*
+ * 9012.cpp
+ *
+ */
+#include <stack>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+bool taken[11];
+bool areFriends[11][11] ;
+int n ,m;
+
+
+int countParings (){
+    
+    int firstFree = -1 ;
+    for (int i = 0 ; i< n ; i++){
+        if ( !taken[i ]){
+            firstFree= i;
+            break ;
+        }
+    }
+    
+    if (firstFree ==-1 ){
+        
+        return 1 ;
+    }
+    
+    int ret = 0 ;
+    for (int j= firstFree+1 ; j<n ;j++){
+        if (!taken [j]  && areFriends [firstFree][j] ){
+            taken[firstFree] =taken[j] = true ;
+            ret += countParings() ;
+            taken[firstFree] =taken[j] = false ;
+        }
+    }
+    return ret ;
+}
+
+
+
+
+int main() {
+    freopen( "input.txt", "r",stdin);
+    int tc ;
+    cin >> tc;
+    while (tc--){
+        memset ( taken , sizeof ( taken ) , false );
+        memset ( areFriends , sizeof ( areFriends ) , false );
+        
+        scanf("%d%d",&n,&m);
+        for (int i = 0 ;i < m; i++){
+            int first, second;
+            scanf("%d%d",&first , & second );
+            areFriends [first][second] = areFriends[second][first] = true ;
+        }
+        cout << countParings() << endl ;
+    }
+    
+    
+    
+    
+}
+
+
